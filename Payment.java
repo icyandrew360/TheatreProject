50cb90cf4f4b807bb6d6aede397f8607d6791bd1
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2022-11-29 13:37:43 -0700

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,1 +1,14 @@
 
+/*
+* File: Payment.java
+* Assignment: ENSF480 Final Project
+* Section: L02
+* Due Date: Dec. 4, 2022
+* Authors: Andrew Howe (30113096), James Platt (30130627), 
+* Jenna Vlaar (30113774) and Sadman Shahriar (30121460)
+*/
+
+public class Payment
+{
+  
+}
