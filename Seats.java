9b01d4902efedb2dda71f3cbabfdf7c45585bb27
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2022-11-29 13:38:33 -0700

    Update Seats.java

--- Seats.java
+++ Seats.java
@@ -1,1 +1,13 @@
+/*
+* File: Seats.java
+* Assignment: ENSF480 Final Project
+* Section: L02
+* Due Date: Dec. 4, 2022
+* Authors: Andrew Howe (30113096), James Platt (30130627), 
+* Jenna Vlaar (30113774) and Sadman Shahriar (30121460)
+*/
 
+public class Seats
+{
+  
+}
