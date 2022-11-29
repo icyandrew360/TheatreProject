689141a46c217c2a7a80050e368f256e430f3822
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2022-11-29 13:36:43 -0700

    Update User.java

--- User.java
+++ User.java
@@ -1,8 +1,10 @@
 /*
+* File: User.java
 * Assignment: ENSF480 Final Project
-* Authors: Andrew Howe, James Platt, Jenna Vlaar and Sadman Shahriar
 * Section: L02
 * Due Date: Dec. 4, 2022
+* Authors: Andrew Howe (30113096), James Platt (30130627), 
+* Jenna Vlaar (30113774) and Sadman Shahriar (30121460)
 */
 
 public class User
