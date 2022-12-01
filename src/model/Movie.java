c6be4582d0a89fcad896a9b85fed2f007dc892f1
Sadman Shahriar <sadmansnigdho@gmail.com>
2022-11-30 19:32:52 -0700

    added package folders

---
+++ src/model/Movie.java
@@ -0,0 +1,24 @@
+package model;
+import java.util.ArrayList;
+
+/*
+* File: Movie.java
+* Assignment: ENSF480 Final Project
+* Section: L02
+* Due Date: Dec. 4, 2022
+* Authors: Andrew Howe (30113096), James Platt (30130627),
+* Jenna Vlaar (30113774) and Sadman Shahriar (30121460)
+*/
+
+
+
+class ShowTime
+{
+  private String time;
+  private ArrayList<Seats> seats;
+}
+
+public class Movie
+{
+  private ArrayList<ShowTime> showTimes;
+}
---
+++ src/model/Ticket.java
@@ -0,0 +1,21 @@
+/*
+* File: Ticket.java
+* Assignment: ENSF480 Final Project
+* Section: L02
+* Due Date: Dec. 4, 2022
+* Authors: Andrew Howe (30113096), James Platt (30130627), 
+* Jenna Vlaar (30113774) and Sadman Shahriar (30121460)
+*/
+
+public class Ticket
+{
+    private int ticketID;
+    private int userID;
+    private Seats seatNumber;
+    private ShowRoom showRoom;
+
+    public Ticket(){
+        
+    }
+  
+}
