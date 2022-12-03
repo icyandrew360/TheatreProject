b06afc6624ef660974896733c10324d867072944
Andrew Howe <andrewhowe1000@gmail.com>
2022-12-02 17:19:48 -0700

    Updated user and user controller

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,11 +1,62 @@
 package controllers;
 
+import model.*;
 import java.sql.*;
+import java.util.HashMap;
 
 public class UserController extends DBController{
    
     public UserController(){
         super("temp url that should be changed");
+    }
+
+    private HashMap<String, String> loginInfos = new HashMap<String, String>();
+
+    private HashMap<String, User> userData = new HashMap<String, User>();
+
+    //this map will be used for validating login info quickly. Use next function for more detailed user info.
+    public void pullLoginInfoValidation(){
+        String sql = "select * from userdb";
+        PreparedStatement p = null;
+        ResultSet rs = null;
+
+        try{
+            p = dbConnect.prepareStatement(sql);
+            rs = p.executeQuery();
+
+            while (rs.next()){
+                loginInfos.put(rs.getString("email"), rs.getString("password"));
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void pullUserInfo(){
+        String sql = "select * from userdb";
+        PreparedStatement p = null;
+        ResultSet rs = null;
+
+        try{
+            p = dbConnect.prepareStatement(sql);
+            rs = p.executeQuery();
+
+            while (rs.next()){
+                User tempUser = new User();
+                tempUser.setFirstName(rs.getString("Fname"));
+                tempUser.setLastName(rs.getString("Lname"));
+                tempUser.setEmail(rs.getString("email"));
+                //this doesn't work yet
+                tempUser.setPhone(rs.getString("phone"));
+                
+                userData.put(tempUser.getEmail(), tempUser);
+            }
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
     //vector<string> of user emails for verifying if someone is a user
     //add users to data base based on 
--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.ArrayList;
 /*
 * File: User.java
 * Assignment: ENSF480 Final Project
@@ -13,10 +14,39 @@ public class User
   private String firstName;
   private String lastName;
   private String email;
-  private Ticket[] tickets;
-  private Payment[] paymentHistory;
+  private ArrayList<Ticket> tickets;
+  private ArrayList<Payment> paymentHistory;
 
   public User(){
-    
+
+  }
+
+  public void addPayment(Payment payment){
+    paymentHistory.add(payment);
+  }
+  public void addTicket(Ticket ticket){
+    tickets.add(ticket);
+  }
+
+  public String getFirstName() {return firstName;}
+  public String getLastName() {return lastName;}
+  public String getEmail() {return email;}
+  public ArrayList<Ticket> getTickets() {return tickets;}
+  public ArrayList<Payment> getPaymentHistory() {return paymentHistory;}
+
+  public void setFirstName(String name){
+    this.firstName = name;
+  }
+  public void setLastName(String name){
+    this.lastName = name;
+  }
+  public void setEmail(String email){
+    this.email = email;
+  }
+  public void setTickets(ArrayList<Ticket> tickets){
+    this.tickets = tickets;
+  }
+  public void setPaymentHistory(ArrayList<Payment> payments){
+    this.paymentHistory = payments;
   }
 }
