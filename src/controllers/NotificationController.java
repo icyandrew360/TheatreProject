6828a08c72efdff84b10f3b07b20b04f06d8eec8
Sadman Shahriar <sadmansnigdho@gmail.com>
2022-12-03 18:58:18 -0700

    added email function

--- src/controllers/NotificationController.java
+++ src/controllers/NotificationController.java
@@ -1,13 +1,46 @@
 package controllers;
 
+import java.util.*;
+import javax.mail.*;
 
+import javax.mail.internet.*;
+import javax.activation.*;
 
 public class NotificationController {
-    private DBController database;
 
-    public NotificationController(){
-        this.database = DBController.getConnection();
+    public void sendMail(String email){
+        String to = email;
+        String from = "jmovies.noreply@gmail.com";
+
+        String host  = "localhost";
+
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.google.com");
+        properties.put("mail.smtp.port", 587);
+
+        Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentiation(){
+                return new PasswordAuthentication("jmovies.noreply@gmail.com", "ensf-480");
+            }
+        });
+        try{
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Payment Successfull");
+            message.setText("Your ticket number is ");
+            Transport.send(message);
+            System.out.println("Email Sent!");
+        }
+        catch(MessagingException mex){
+            mex.printStackTrace();
+        }
+
+
+
     }
-
-
 }
