afbcfbfa34c27249362dce50f97094984687c004
Platty114 <jamesplatt02@gmail.com>
2022-12-03 20:56:18 -0700

    Update TicketController and UserController

--- src/controllers/TicketController.java
+++ src/controllers/TicketController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import model.*;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Vector;
+import java.util.ArrayList;
 
 public class TicketController {
     private DBController onlyInstance;
@@ -9,4 +12,28 @@ public class TicketController {
     public TicketController(){
        this.onlyInstance = DBController.getConnection();
     }
+
+    // public void populateTickets(HashMap<String, ArrayList<Ticket>> map){
+    //     String sql = "select * from tickets";
+    //     PreparedStatement p = null;
+    //     ResultSet rs = null;
+
+    //     try{
+    //         p = this.onlyInstance.getDBConnection().prepareStatement(sql);
+    //         rs = p.executeQuery();
+
+    //         while (rs.next()){
+    //             if(map.get(rs.getString("email")) != null){
+    //                 map.get(rs.getString("email")).add(new Ticket(rs.getString("ID"), rs.getString("email")
+    //                 , rs.getString("seatNumber"), rs.getString(""), int showTime, String showDate, String movieName));
+    //             }
+    //         }
+
+    //     }
+    //     catch(Exception e){
+    //         e.printStackTrace();
+    //     }
+    // }
+
+
 }
