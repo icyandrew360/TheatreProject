ec1da684c5b5b95673c3c854286a2ddd9e228378
Andrew Howe <andrewhowe1000@gmail.com>
2022-12-03 21:29:50 -0700

    showcontroller now returns "noDecimalsLand"

--- src/controllers/ShowController.java
+++ src/controllers/ShowController.java
@@ -63,6 +63,6 @@ public class ShowController {
             }
         }
         //if we didnt find a match:
-        return searchableMovieList.get(0);
+        return ("noDecimalsLand");
     }
 }
